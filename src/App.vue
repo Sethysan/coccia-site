e71b53dbe1154839131f2497cc761539ba153b3f
Sethysan <seth.r.drummage@gmail.com>
2026-06-23 12:29:19 -0400

    landing pages css

--- src/App.vue
+++ src/App.vue
@@ -10,13 +10,21 @@ import SiteHeader from './components/SiteHeader.vue'
 import { RouterView } from 'vue-router'
 </script>
 
-<style scoped>
+<style scoped >
 .content {
   padding-top: 90px;
+  min-height: 75vh;
+  background-image: url('./assets/pizza.jpg');
+  background-size: contain;
+  background-color: #111;
+  background-position: center;
+  background-repeat: no-repeat;
 }
-
 div {
   font-family: system-ui, 'Segoe UI', Roboto, sans-serif;
+
+  
 }
+
 
 </style>
--- src/Views/AboutVIew.vue
+++ src/Views/AboutVIew.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>
-    <h1>About</h1>
+  <div id="about">
+    <div class="header">
+      <h2 >About</h2>
+    </div>
+    <div class="body">
+      <p>Established in 1958 by Minnie epsom larum onum</p>
+    </div>
   </div>
 </template>
+
+<style scoped>
+
+#about {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.header {
+  font-size: 3em ;
+}
+</style>
